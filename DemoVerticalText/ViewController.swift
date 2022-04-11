3cffc942903be72b0f31ddc90924819ffd4807eb
朱旭宏 <1906457616@qq.com>
2022-04-11 10:18:20 +0800

    Added fonts.

--- DemoVerticalText/ViewController.swift
+++ DemoVerticalText/ViewController.swift
@@ -39,11 +39,19 @@ class ViewController: UIViewController {
     @IBAction func breakingModeChanged(_ sender: UISegmentedControl) {
         label.breakingMode = sender.selectedSegmentIndex
     }
+    @IBAction func fontChanged(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            label.font = .fangZhengShuSong(24)
+        }
+        else {
+            label.font = .huaWenLiShu(24)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        label.font = .boldSystemFont(ofSize: 24)
+        label.font = .fangZhengShuSong(24)
 //        label.text = String(repeating: "桃红樱粉玉兰香，\n清明过后各自藏。\n来年若要赏春光，\n长安二月君堪访。", count: 1)
 //        label.text = String(repeating: "Hello World! \n", count: 5)
         label.text = "东风夜放花千树，\n更吹落，星如雨。\n宝马雕车香满路，\n凤箫声动，玉壶光转，\n一夜鱼龙舞。\n\n\n\n\n蛾儿雪柳黄金缕，\n笑语盈盈暗香去。\n众里寻他千百度，\n蓦然回首，\n那人却在，灯火阑珊处。这是超出的文本这是超出的文本这是超出的文本这是超出的文本"
@@ -53,6 +61,15 @@ class ViewController: UIViewController {
         label.setNeedsUpdate()
     }
 
+}
+
+extension UIFont {
+    static func fangZhengShuSong(_ size: CGFloat) -> UIFont {
+        return .init(name: "FZShuSong-Z01S", size: 24)!
+    }
+    static func huaWenLiShu(_ size: CGFloat) -> UIFont {
+        return .init(name: "STLiti", size: 24)!
+    }
 }
 
 extension UIColor {
