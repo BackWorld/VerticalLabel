315995df0cfff5f709f334ea9df3ea28070a8832
朱旭宏 <1906457616@qq.com>
2022-04-11 11:39:31 +0800

    Updated Demos.

--- DemoVerticalText/ViewController.swift
+++ DemoVerticalText/ViewController.swift
@@ -54,7 +54,8 @@ class ViewController: UIViewController {
         label.font = .fangZhengShuSong(24)
 //        label.text = String(repeating: "桃红樱粉玉兰香，\n清明过后各自藏。\n来年若要赏春光，\n长安二月君堪访。", count: 1)
 //        label.text = String(repeating: "Hello World! \n", count: 5)
-        label.text = "东风夜放花千树，\n更吹落，星如雨。\n宝马雕车香满路，\n凤箫声动，玉壶光转，\n一夜鱼龙舞。\n\n\n\n\n蛾儿雪柳黄金缕，\n笑语盈盈暗香去。\n众里寻他千百度，\n蓦然回首，\n那人却在，灯火阑珊处。这是超出的文本这是超出的文本这是超出的文本这是超出的文本"
+//        label.text = "东风夜放花千树，\n更吹落，星如雨。\n宝马雕车香满路，\n凤箫声动，玉壶光转，\n一夜鱼龙舞。\n\n\n\n\n蛾儿雪柳黄金缕，\n笑语盈盈暗香去。\n众里寻他千百度，\n蓦然回首，\n那人却在，灯火阑珊处。这是超出的文本这是超出的文本这是超出的文本这是超出的文本"
+        label.text = "春有百花秋有月，\n夏有凉风冬有雪。\n若无闲事挂心头，\n便是人间好时节。"
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
